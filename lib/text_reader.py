63d781a569f61400041e65c29a9605bcd40b287e
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-11-15 12:29:13 +0000

    all text_reader tests passing

--- lib/text_reader.py
+++ lib/text_reader.py
@@ -1,9 +1,22 @@
 import math
 
 def text_reader(text):
+    is_string = isinstance(text, str)
+
+    if is_string == False:
+        raise Exception("Input value must be a string")
+    
     text_length = len(text.split())
 
-    if text_length >= 200:
+    if text == '':
+        print("It would take you 0 minutes to read this text.")
+        return "It would take you 0 minutes to read this text."
+
+    elif text_length >= 200:
         num_mins = int(math.ceil(text_length/200))
         print(f"It would take you {num_mins} minutes to read this text.")
         return f"It would take you {num_mins} minutes to read this text."
+
+    else:
+        print("It would take you less than 1 minute to read this text.")
+        return "It would take you less than 1 minute to read this text."
--- tests/test_text_reader.py
+++ tests/test_text_reader.py
@@ -8,3 +8,17 @@ def test_text_reader_returns_2_for_400():
 def test_text_reader_returns_4_for_700():
     result = text_reader('one '*700)
     assert result == "It would take you 4 minutes to read this text."
+
+def test_text_reader_returns_less_than_1_for_150():
+    result = text_reader('one '*150)
+    assert result == "It would take you less than 1 minute to read this text."
+
+def test_text_reader_returns_0_for_empty():
+    result = text_reader('')
+    assert result == "It would take you 0 minutes to read this text."
+
+def test_text_reader_throws_error_for_list():
+    with pytest.raises(Exception) as e:
+        text_reader(['house'])
+    error_message = str(e.value)
+    assert error_message == "Input value must be a string"
